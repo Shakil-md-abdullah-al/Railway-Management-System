7f95405d4e57ecb8811212455fcde128e0db1de4
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-03-16 00:26:33 +0600

    Replace Updated

--- Rail Sheba.c
+++ Rail Sheba.c
@@ -71,3 +71,51 @@ int delete_file_row(FILE *main_file, FILE *temp_file, const int delete_line)
 //Update or Replace File Row
 
 /*Home End*/
+int replace_row(char filename[], int delete_line)
+{
+    FILE *main_file;
+    FILE *temp_file;
+    FILE *repl_file;
+
+    int count=1;
+    char buffer[255];
+    char replace[255];
+
+    main_file = fopen(filename, "r");
+    repl_file = fopen("rep-file.tmp", "r");
+    temp_file = fopen("temp-file.tmp", "w");
+
+    if (main_file == NULL || temp_file == NULL || repl_file == NULL)
+    {
+        printf("\n\nUnable to open file.\n");
+        exit(1);
+    }
+
+    fgets(replace, 250, repl_file);
+
+    rewind(main_file);
+
+    while ((fgets(buffer, 250, main_file)) != NULL)
+    {
+        if (delete_line != count)
+            fputs(buffer, temp_file);
+        else
+            fputs(replace, temp_file);
+
+        count++;
+    }
+
+    fclose(main_file);
+    fclose(temp_file);
+
+    remove(filename);
+    remove("rep-file.tmp");
+
+    rename("temp-file.tmp", filename);
+
+    printf("\n\n\t\t\tUpdated List (%d no Row).\n\n", delete_line);
+    main_file = fopen(filename, "r");
+    print_file(main_file);
+
+    fclose(main_file);
+}
