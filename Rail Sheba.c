3255687c214d75f2a52c7f7c58fe31ad19307d26
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-03-16 00:25:09 +0600

    Add Home Menu

--- Rail Sheba.c
+++ Rail Sheba.c
@@ -3,3 +3,44 @@
 #include<string.h>
 #include<time.h>
 #include<windows.h>
+
+
+/*Home Start*/
+int home()
+{
+    system("COLOR 07");
+    printf("\n\t\t\t\t\t\t::::::::::::::::::::::::::::::::::::::::::::\n");
+    printf("\t\t\t\t\t\t::                                        ::\n");
+    printf("\t\t\t\t\t\t::::::::::       Home Menu       :::::::::::\n");
+    printf("\t\t\t\t\t\t::                                        ::\n");
+    printf("\t\t\t\t\t\t::::::::::::::::::::::::::::::::::::::::::::\n\n\n");
+    printf("\t\t\t\t\t\tSelect an Option From Below:\n");
+    printf("\t\t\t\t\t\t1. ADMIN LOGIN\n");
+    printf("\t\t\t\t\t\t2. USER LOGIN\n");
+    printf("\t\t\t\t\t\t3. OPTION\n");
+    int ch;
+    printf("\n\t\t\t\t\t\tEnter Your Choice: ");
+    scanf("%d", &ch);
+    switch(ch)
+    {
+    case 1:
+        Admin_menu(); // Log in Replace
+        break;
+    case 2:
+        User_menu();
+        break;
+    case 3:
+        option();
+        break;
+
+    default:
+        printf("\a\n\t\t\t\t\t\tWrong Input\n");
+        printf("\t\t\t\t\t\tPress any key to go back.");
+        getch();
+        system("CLS");
+        home();
+    }
+
+}
+
+/*Home End*/
